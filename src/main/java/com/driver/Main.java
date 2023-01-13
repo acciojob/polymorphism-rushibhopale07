acd60f09b5059e6dff65bcf7cb757fd533a60cf0
Rushi <rushibhopale15@gmail.com>
2023-01-13 15:48:37 +0530

    Task complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args)
+    {
+        Product p= new Product();
+        p.product(5, 6);
+        p.product(4,5,6);
+        p.product(3.4,5.6);
+    }
+}
+class Product
+{
+    public int product(int x, int y) {
+        return x*y;
+    }
+    public int product(int x, int y, int z) {
+
+        return x*y*z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
 }
